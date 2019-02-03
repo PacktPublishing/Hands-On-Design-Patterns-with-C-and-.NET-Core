431c63f7f2b5e908a30140f0b21711e83a71c34a
Gaurav Aroraa <g_arora@hotmail.com>
2019-02-04 03:17:38 +0530

    started CRUDS

--- Chapter6/FlixOne/FlixOne.Web/Controllers/CategoryController.cs
+++ Chapter6/FlixOne/FlixOne.Web/Controllers/CategoryController.cs
@@ -1,31 +1,29 @@
 ﻿using System;
-using FlixOne.Web.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlixOne.Web.Controllers
 {
-    public class ProductController : Controller
+    public class CategoryController: Controller
     {
-       
         public IActionResult Index()
         {
             return View();
         }
 
-        
+
         public IActionResult Details(Guid id)
         {
             return View();
         }
 
-       
+
         public IActionResult Create()
         {
             return View();
         }
 
-        
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(IFormCollection collection)
@@ -42,8 +40,8 @@ namespace FlixOne.Web.Controllers
             }
         }
 
-       
-        public IActionResult Edit(int id)
+
+        public IActionResult Edit(Guid id)
         {
             return View();
         }
@@ -51,7 +49,7 @@ namespace FlixOne.Web.Controllers
         // POST: Default/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, IFormCollection collection)
+        public IActionResult Edit(Guid id, IFormCollection collection)
         {
             try
             {
@@ -65,14 +63,14 @@ namespace FlixOne.Web.Controllers
             }
         }
 
-       public IActionResult Delete(int id)
+        public IActionResult Delete(Guid id)
         {
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Delete(int id, IFormCollection collection)
+        public IActionResult Delete(Guid id, IFormCollection collection)
         {
             try
             {
@@ -84,33 +82,6 @@ namespace FlixOne.Web.Controllers
             {
                 return View();
             }
-        }
-
-        private Product ToProductModel(ProductViewModel productvm)
-        {
-            return new Product
-            {
-                CategoryId = productvm.CategoryId,
-                Description = productvm.ProductDescription,
-                Id = productvm.ProductId,
-                Name = productvm.ProductName,
-                Price = productvm.ProductPrice
-            };
-        }
-
-        private ProductViewModel ToProductvm(Product productModel)
-        {
-            return new ProductViewModel
-            {
-                CategoryId = productModel.CategoryId,
-                CategoryDescription = productModel.Category.Description,
-                CategoryName = productModel.Category.Name,
-                ProductDescription = productModel.Description,
-                ProductId = productModel.Id,
-                ProductImage = productModel.Image,
-                ProductName = productModel.Name,
-                ProductPrice = productModel.Price
-            };
         }
     }
 }
