c011334a3b3e1d4609f7b958f351801d1684755f
Gaurav Aroraa <g_arora@hotmail.com>
2019-02-04 02:22:02 +0530

    added ProductController

---
+++ Chapter6/FlixOne/FlixOne.Web/Controllers/ProductController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FlixOne.Web.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace FlixOne.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Default
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Default/Details/5
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Default/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Default/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Default/Edit/5
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Default/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Default/Delete/5
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Default/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        private Product ToProductModel(ProductViewModel productvm)
+        {
+            return new Product
+            {
+                CategoryId = productvm.CategoryId,
+                Description = productvm.ProductDescription,
+                Id = productvm.ProductId,
+                Name = productvm.ProductName,
+                Price = productvm.ProductPrice
+            };
+        }
+
+        private ProductViewModel ToProductvm(Product productModel)
+        {
+            return new ProductViewModel
+            {
+                CategoryId = productModel.CategoryId,
+                CategoryDescription = productModel.Category.Description,
+                CategoryName = productModel.Category.Name,
+                ProductDescription = productModel.Description,
+                ProductId = productModel.Id,
+                ProductImage = productModel.Image,
+                ProductName = productModel.Name,
+                ProductPrice = productModel.Price
+            };
+        }
+    }
+}
