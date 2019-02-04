d20f167ad9553371f2a6fe78b6539f5ee9b197c9
Gaurav Aroraa <g_arora@hotmail.com>
2019-02-04 07:18:00 +0530

    wrapping up chapter 06

--- Chapter6/FlixOne/FlixOne.Web/Startup.cs
+++ Chapter6/FlixOne/FlixOne.Web/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using FlixOne.Web.Contexts;
+﻿using FlixOne.Web.Contexts;
 using FlixOne.Web.Persistance;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
