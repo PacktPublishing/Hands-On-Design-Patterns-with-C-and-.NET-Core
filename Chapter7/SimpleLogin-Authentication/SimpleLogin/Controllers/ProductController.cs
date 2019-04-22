31596eaedb96f2a975dbc353dd3b9c39155fc2f8
Gaurav Aroraa <g_arora@hotmail.com>
2019-04-22 17:14:55 +0530

    added simplelogin project

---
+++ Chapter7/SimpleLogin-Authentication/SimpleLogin/Controllers/ProductController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using SimpleLogin.Common;
+using SimpleLogin.Models;
+using SimpleLogin.Persistance;
+
+namespace SimpleLogin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IInventoryRepositry _inventoryRepositry;
+
+        public ProductController(IInventoryRepositry inventoryRepositry) => _inventoryRepositry = inventoryRepositry;
+
+        public IActionResult Index() => View(_inventoryRepositry.GetProducts().ToProductvm());
+
+        public IActionResult Details(Guid id) => View(_inventoryRepositry.GetProduct(id).ToProductvm());
+        
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([FromBody] Product product)
+        {
+            try
+            {
+                _inventoryRepositry.AddProduct(product);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        public IActionResult Edit(Guid id) => View(_inventoryRepositry.GetProduct(id));
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Guid id, [FromBody] Product product)
+        {
+            try
+            {
+                _inventoryRepositry.UpdateProduct(product);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Delete(Guid id) => View(_inventoryRepositry.GetProduct(id));
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Guid id, [FromBody] Product product)
+        {
+            try
+            {
+                _inventoryRepositry.RemoveProduct(product);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Chapter7/SimpleLogin-Authentication/SimpleLogin/Models/ProductViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SimpleLogin.Models
+{
+    public class ProductViewModel
+    {
+        public Guid ProductId { get; set; }
+        [DisplayName("Product Name")]
+        public string ProductName { get; set; }
+        [DisplayName("Description")]
+        public string ProductDescription { get; set; }
+        [DisplayName("Image")]
+        public string ProductImage { get; set; }
+        [DisplayName("Price (INR)")]
+        public decimal ProductPrice { get; set; }
+        public Guid CategoryId { get; set; }
+        [DisplayName("Category Name")]
+        public string CategoryName { get; set; }
+        [DisplayName("Product Desc.")]
+        public string CategoryDescription { get; set; }
+    }
+}
