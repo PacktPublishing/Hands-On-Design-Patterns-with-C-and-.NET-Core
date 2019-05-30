3dd915893f77698638d1d275337b500e2b9efeb9
Gaurav Aroraa <g_arora@hotmail.com>
2019-05-30 21:47:11 +0530

    Chapter #8 dead lock situation

---
+++ Chapter8/BestPractices/BestPractices/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace BestPractices
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Starts here...");
+            DeadLock();
+            Console.ReadKey();
+        }
+
+        private static void DeadLock()
+        {
+            var ticketCounter = new TicketCounter();
+            var counterA = new Thread(ticketCounter.ShowMessage);
+            var counterB = new Thread(ticketCounter.ShowMessage);
+            var counterC = new Thread(ticketCounter.ShowMessage);
+            counterA.Name = "A";
+            counterB.Name = "B";
+            counterC.Name = "C";
+            //counterC.Priority = ThreadPriority.Highest;
+            //counterB.Priority = ThreadPriority.Normal;
+            //counterA.Priority = ThreadPriority.Lowest;
+            counterA.Start();
+            counterB.Start();
+            counterC.Start();
+        }
+    }
+}
---
+++ Chapter8/BestPractices/BestPractices/TicketCounter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace BestPractices
+{
+    internal class TicketCounter
+    {
+        private static readonly object Object = new object();
+
+        public void ShowMessage()
+        {
+            const int personsInQueue = 5;
+            if (Monitor.TryEnter(Object, 300))
+            {
+                try
+                {
+                    var thread = Thread.CurrentThread;
+                    for (var personCount = 0; personCount < personsInQueue; personCount++)
+                        Console.WriteLine(
+                            $"\tPerson {personCount + 1} is collecting ticket from counter {thread.Name}.");
+                }
+                finally
+                {
+                    Monitor.Exit(Object);
+                }
+            }
+            else
+            {
+                Console.WriteLine(
+                    $"This statement will execute if the attempt times out to acquire an exclusive lock on {ToString()}   ");
+                var thread = Thread.CurrentThread;
+                for (var personCount = 0; personCount < personsInQueue; personCount++)
+                    Console.WriteLine(
+                        $"\tPerson {personCount + 1} is collecting ticket from counter {thread.Name}.");
+            }
+        }
+    }
+}
