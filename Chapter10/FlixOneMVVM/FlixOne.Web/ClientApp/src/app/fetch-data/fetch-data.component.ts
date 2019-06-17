febbb96282c62fc9181e8844857524147abaac9d
Gaurav Aroraa <g_arora@hotmail.com>
2019-06-17 12:00:24 +0530

    Changes for MVVM pattern

--- Chapter10/FlixOneMVVM/FlixOne.Web/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ Chapter10/FlixOneMVVM/FlixOne.Web/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -6,18 +6,18 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+  public forecasts: Products[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
+    http.get<Products[]>(baseUrl + 'api/SampleData/GetProducts').subscribe(result => {
       this.forecasts = result;
     }, error => console.error(error));
   }
 }
 
-interface WeatherForecast {
-  dateFormatted: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+interface Products {
+  productName: string;
+  catname: string;
+  productPrice: number;
+  productDescription: string;
 }
--- Chapter10/FlixOneMVVM/FlixOne.Web/Controllers/SampleDataController.cs
+++ Chapter10/FlixOneMVVM/FlixOne.Web/Controllers/SampleDataController.cs
@@ -25,7 +25,42 @@ namespace FlixOne.Web.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             });
         }
+        [HttpGet("[action]")]
+        public IEnumerable<Product> GetProducts()
+        {
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    ProductId = Guid.NewGuid(),
+                    ProductName = "Microservices Book",
+                    ProductDescription = "Building Microservices with .NET Core2.0",
+                    ProductPrice = 655,
+                    CategoryDescription = "Books"
+                },
+                new Product
+                {
+                    ProductId = Guid.NewGuid(),
+                    ProductName = "Mango",
+                    ProductDescription = "A juicy mango",
+                    ProductPrice = 135,
+                    CategoryDescription = "Fruits"
+                }
+            };
+            return products;
+        }
 
+        public class Product
+        {
+            public Guid ProductId { get; set; }
+            public string ProductName { get; set; }
+            public string ProductDescription { get; set; }
+            public string ProductImage { get; set; }
+            public decimal ProductPrice { get; set; }
+            public Guid CategoryId { get; set; }
+            public string CategoryName { get; set; }
+            public string CategoryDescription { get; set; }
+        }
         public class WeatherForecast
         {
             public string DateFormatted { get; set; }
