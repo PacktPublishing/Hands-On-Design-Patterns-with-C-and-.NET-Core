4f08deefe65443712bb25a917baab2ed7c2c6fce
chilberto <chilberto@yahoo.com>
2019-06-19 14:34:57 +1200

    Chapter 2 Observer Design Pattern added

---
+++ Chapter2/ObserverPattern/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ObserverPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var subject = new Subject();
+            var greenObserver = new Observer(ConsoleColor.Green);
+            var redObserver = new Observer(ConsoleColor.Red);
+            var yellowObserver = new Observer(ConsoleColor.Yellow);
+
+            subject.OnQuantityUpdated += greenObserver.ObserverQuantity;
+            subject.OnQuantityUpdated += redObserver.ObserverQuantity;
+            subject.OnQuantityUpdated += yellowObserver.ObserverQuantity;
+
+            subject.UpdateQuantity(12);
+            subject.UpdateQuantity(5);            
+
+            Console.WriteLine("Enter a key to quit.");
+            Console.Read();
+        }
+    }
+
+    public delegate void QuantityUpdated(int quantity);
+
+    class Subject
+    {
+        private int _quantity = 0;
+        
+        public event QuantityUpdated OnQuantityUpdated;
+
+        public void UpdateQuantity(int value)
+        {
+            _quantity += value;
+
+            // alert any observers
+            OnQuantityUpdated?.Invoke(_quantity);
+
+        }
+    }
+
+    class Observer
+    {
+        ConsoleColor _color;
+        public Observer(ConsoleColor color)
+        {
+            _color = color;
+        }
+
+        internal void ObserverQuantity(int quantity)
+        {
+            Console.ForegroundColor = _color;
+            Console.WriteLine($"I observer the new quantity value of {quantity}.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
